33e8ccf30474c8cdf4dd291e2a1d96cf2b92e1f6
jakeogh <github.com@v6y.net>
2020-01-19 10:39:02 -0700

    auto-commit

--- symlinktree/symlinktree.py
+++ symlinktree/symlinktree.py
@@ -23,6 +23,7 @@ import time
 import shutil
 import click
 from kcl.printops import ceprint
+from kcl.printops import eprint
 from kcl.symlinkops import is_broken_symlink
 from kcl.symlinkops import is_unbroken_symlink
 from kcl.symlinkops import symlink_destination
@@ -38,7 +39,7 @@ ic.lineWrapWidth, _ = get_terminal_size((80, 20))
 
 
 def path_exists(path): #gak, misses symlinks
-    ceprint("path:", path)
+    ic(path)
     if isinstance(path, str): #filter out bool or int to not bit by os.stat(False) or os.stat(0)
         if os.path.lexists(path):
             return True
@@ -52,8 +53,8 @@ def mkdir_or_exit(folder):
     try:
         os.makedirs(folder)
     except Exception as e:
-        print("Exception: %s", e)
-        print("Unable to os.mkdir(%s). Exiting.", folder)
+        eprint("Exception: %s", e)
+        eprint("Unable to os.mkdir(%s). Exiting.", folder)
         os._exit(1)
 
 
@@ -65,58 +66,66 @@ def cli(sysskel):
     assert path_is_dir(sysskel_dir)
 
     if not os.path.exists(sysskel_dir):
-        print("sysskel_dir:", sysskel_dir, "does not exist. Exiting.")
+        eprint("sysskel_dir:", sysskel_dir, "does not exist. Exiting.")
         os._exit(1)
 
     #orig_file_list = [os.path.join(path, filename) for path, dirs, files in os.walk(sysskel_dir) for filename in files]
     file_list = files(sysskel_dir)
 
     skip_list = []
+    skip_dirs = set()
     for infile in file_list:
-        skip_current = False
-        print("\ninfile:", infile)
-        if infile.pathlib.name == '.symlink_dir':
-            print("skipping .symlink_dir dotfile")
-            continue
-        for item in skip_list:
-            if str(infile.pathlib).startswith(item):
-                print("skip_list skipping:", infile)
-                skip_current = True
-                break
-        if skip_current:
-            continue
-        if path_is_dir(infile):
-            #print("found directory:", infile)
-            #print("checking if it contains .symlink_dir")
-            if file_exists(infile + '/.symlink_dir'):
-                print(".symlink_dir found")
-                print("adding", infile, "to skip_list")
-                skip_list.append(infile + '/')
-            else:
-                print("skipping directory:", infile)
-                continue
-
+        infile = infile.pathlib
+        eprint("\ninfile:", infile)
         timestamp = str(time.time())
-        dest_dir = '/' + '/'.join(str(infile.pathlib).split('/')[4:-1])
-        if is_unbroken_symlink(dest_dir):
-            print("dest_dir is a unbroken symlink, checking if it points to the infiles own dir")
-            dest_dir_symlink_destination = symlink_destination(dest_dir)
-            infile_folder = '/'.join(str(infile.pathlib).split('/')[0:-1])
-            print("dest_dir_symlink_destination:", dest_dir_symlink_destination)
-            print("infile_folder:", infile_folder)
-            try:  #bug papered over, remove the try
-                assert not dest_dir_symlink_destination == infile_folder
-            except AssertionError:
-                continue
 
-        dest_file = '/' + '/'.join(str(infile.pathlib).split('/')[4:])
-        print("dest_file:", dest_file)
-        #continue
+        if infile.parent in skip_dirs:
+            continue
+
+        #skip_current = False
+
+        if infile.name == '.symlink_dir':
+            skip_dirs.add(infile.parent)
+            print("found .symlink_dir dotfile:", infile)
+            continue
+
+        #for item in skip_list:
+        #    if str(infile.pathlib).startswith(item):
+        #        print("skip_list skipping:", infile)
+        #        skip_current = True
+        #        break
+        #if skip_current:
+        #    continue
+
+        #if path_is_dir(infile):  # never happens
+        #    #print("found directory:", infile)
+        #    #print("checking if it contains .symlink_dir")
+        #    if file_exists(infile + '/.symlink_dir'):
+        #        print(".symlink_dir found")
+        #        print("adding", infile, "to skip_list")
+        #        skip_list.append(infile + '/')
+        #    else:
+        #        print("skipping directory:", infile)
+        #        continue
+
+        dest_dir = '/' + '/'.join(str(infile).split('/')[4:-1])
+        ic(dest_dir)
+        if is_unbroken_symlink(dest_dir):
+            eprint("dest_dir is a unbroken symlink, checking if it points to the infiles own dir")
+            dest_dir_symlink_destination = symlink_destination(dest_dir)
+            infile_folder = '/'.join(str(infile).split('/')[0:-1])
+            ic(dest_dir_symlink_destination)
+            ic(infile_folder)
+            #try:  #bug papered over, remove the try
+            assert not dest_dir_symlink_destination == infile_folder
+            #except AssertionError:
+            #    continue
+
+        dest_file = '/' + '/'.join(str(infile).split('/')[4:])
+        ic(dest_file)
         if is_broken_symlink(dest_file):
             print("found broken symlink at dest_file:", dest_file, "moving it to .old")
             shutil.move(dest_file, dest_file + '.old.' + timestamp)
-            #quit(1)
-
         elif is_unbroken_symlink(dest_file):
             print("skipping pre-existing dest file")
             continue
@@ -127,17 +136,15 @@ def cli(sysskel):
                 shutil.move(dest_file, dest_file + '.old.' + timestamp)
                 #quit(1)
             except Exception as e:
-                print(e)
-                pass
-    ##          print("Problem moving existing file to backup file, exiting")
-    ##          os._exit(1)
+                eprint(e)
+                eprint("Problem moving existing file to backup file, exiting")
+                os._exit(1)
 
         if not path_exists(dest_dir):
             print("making dest_dir:", dest_dir)
             mkdir_or_exit(dest_dir)
 
         symlink_or_exit(infile, dest_file)
-    pass
 
 
 if __name__ == "__main__":
